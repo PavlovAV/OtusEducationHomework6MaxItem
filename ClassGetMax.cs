dc658555f030cd7ca1598679d78e057560c9fe04
PavlovAV <a.pavlov@sgbi.ru>
2023-05-06 22:13:11 +0300

    Add project files.

---
+++ ClassGetMax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusEducationHomework6
+{
+    public static class ClassGetMax<T> where T : class
+    {
+        public static T GetMax(IEnumerable e, Func<T, float> getParameter)
+        {
+            T? maxItem = null;
+            foreach (var item in e) 
+            {
+                if (maxItem == null || getParameter(maxItem) < getParameter((T)item))
+                    maxItem = (T)item;
+            }
+            return (T)maxItem;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OtusEducationHomework6;
+using System.Collections;
+
+List<MyClass> ListMyClass = new List<MyClass> ();
+for (var i= 0; i< 10; i++)
+{
+    ListMyClass.Add(new MyClass(i));
+}
+
+var maxItem = ClassGetMax<MyClass>.GetMax(ListMyClass, GetParameter);
+Console.WriteLine($"Maximum - {maxItem.I}");
+
+static float GetParameter(MyClass item)
+{
+    return (float)item.I;
+}
+
+
+public class MyClass
+{
+    public int I;
+    public MyClass(int i)
+    {
+        I = i;
+    }
+}
